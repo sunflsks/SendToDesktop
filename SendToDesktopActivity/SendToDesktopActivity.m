d896feb6ddc38ae5770fb17486c8cf806772cb4a
Sudhip Nashi <sudhip@nashi.us>
2021-01-20 16:15:08 -0600

    send file on background queue

--- SendToDesktopActivity/SendToDesktopActivity.m
+++ SendToDesktopActivity/SendToDesktopActivity.m
@@ -48,63 +48,65 @@ static inline void TimeLog(NSString* x) {
 }
 
 -(void)performActivity {
-    NSDictionary* prefs = dictWithPreferences();
-    int counter = 0;
-
-    if (![SunflsksNetwork checkIfConnected]) {
-        TimeLog(@"Could not connect to network. Exiting");
-        return;
-    }
-
-    NMSSHSession* remote = [NMSSHSession connectToHost:prefs[@"hostname"] withUsername:prefs[@"username"]];
-
-    if (!remote.isConnected) {
-        TimeLog(@"Could not connect to remote. Exiting");
-        return;
-    }
-
-    [remote authenticateByPassword:prefs[@"password"]];
-
-    if (!remote.isAuthorized) {
-        TimeLog(@"Incorrect credentials. Exiting");
-        return;
-    }
-
-    TimeLog(@"Connected to remote!");
-
-    [remote.sftp connect];
-    [remote.sftp createDirectoryAtPath:prefs[@"directory"]];
-
-    for (id object in items) {
-        NSData* dataToSend = nil;
-        NSMutableString* remoteFileName = [NSMutableString string];
-
-        [remoteFileName appendString:[NSString stringWithFormat:@"%@/", prefs[@"directory"]]];
-
-        if ([object isKindOfClass:[UIImage class]]) {
-            dataToSend = UIImageJPEGRepresentation(object, 0);
-            [remoteFileName appendString: [NSString stringWithFormat:@"IMG-%d.jpg", counter]];
-            counter++;
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
+        NSDictionary* prefs = dictWithPreferences();
+        int counter = 0;
+        if (![SunflsksNetwork checkIfConnected]) {
+            TimeLog(@"Could not connect to network. Exiting");
+            return;
         }
 
-        else if ([object isKindOfClass:[NSURL class]]) {
-            dataToSend = [[NSData alloc] initWithContentsOfURL:object];
-            [remoteFileName appendString:[object lastPathComponent]];
+        NMSSHSession* remote = [NMSSHSession connectToHost:prefs[@"hostname"] withUsername:prefs[@"username"]];
+
+        if (!remote.isConnected) {
+            TimeLog(@"Could not connect to remote. Exiting");
+            return;
         }
 
-        TimeLog([NSString stringWithFormat:@"Saving to remote file with name %@", remoteFileName]);
+        [remote authenticateByPassword:prefs[@"password"]];
 
-        if ([remote.sftp writeContents:dataToSend toFileAtPath:remoteFileName] == NO) {
-            TimeLog(@"Couldn't write contents to remote.");
+        if (!remote.isAuthorized) {
+            TimeLog(@"Incorrect credentials. Exiting");
+            [remote disconnect];
+            return;
         }
 
-        else {
-            TimeLog(@"Wrote contents succesfully!");
-        }
-    }
+        TimeLog(@"Connected to remote!");
 
-    [remote disconnect];
-    TimeLog(@"Disconnected from remote");
+        [remote.sftp connect];
+        [remote.sftp createDirectoryAtPath:prefs[@"directory"]];
+
+        for (id object in items) {
+            NSData* dataToSend = nil;
+            NSMutableString* remoteFileName = [NSMutableString string];
+
+            [remoteFileName appendString:[NSString stringWithFormat:@"%@/", prefs[@"directory"]]];
+
+            if ([object isKindOfClass:[UIImage class]]) {
+                dataToSend = UIImageJPEGRepresentation(object, 0);
+                [remoteFileName appendString: [NSString stringWithFormat:@"IMG-%d.jpg", counter]];
+                counter++;
+            }
+
+            else if ([object isKindOfClass:[NSURL class]]) {
+                dataToSend = [[NSData alloc] initWithContentsOfURL:object];
+                [remoteFileName appendString:[object lastPathComponent]];
+            }
+
+            TimeLog([NSString stringWithFormat:@"Saving to remote file with name %@", remoteFileName]);
+
+            if ([remote.sftp writeContents:dataToSend toFileAtPath:remoteFileName] == NO) {
+                TimeLog(@"Couldn't write contents to remote.");
+            }
+
+            else {
+                TimeLog(@"Wrote contents succesfully!");
+            }
+        }
+
+        [remote disconnect];
+        TimeLog(@"Disconnected from remote");
+    });
 }
 
 @end
