974d636cd71f274703c66696a91b09f3b40dbea5
Sudhip Nashi <sudhip@nashi.us>
2021-01-26 11:09:43 -0600

    Add inline function for easier spawning block on main thread

--- Utils/Utils.h
+++ Utils/Utils.h
@@ -7,3 +7,7 @@ NSString* getPassword(void);
 static inline void TimeLog(NSString* x) {
     Log(stringWithTimestamp(x));
 }
+
+static inline void spawn_on_main_thread(void(^blockus)(void)) {
+    dispatch_async(dispatch_get_main_queue(),blockus);
+}
