a63e1e1c7c3e527621e201d5832af5f7cd4e876b
Sudhip Nashi <sudhip@nashi.us>
2021-01-25 22:41:25 -0600

    remote sizeptr: it was a crappy idea anyway

--- FileSender/FileSender.h
+++ FileSender/FileSender.h
@@ -1,9 +1,9 @@
 @interface FileSender : NSObject
 -(id)init;
 -(BOOL)sendURL:(NSURL*)url;
--(BOOL)sendURL:(NSURL*)url progress:(BOOL (^)(NSUInteger))progress sizeptr:(NSUInteger*)sizeptr;
+-(BOOL)sendURL:(NSURL*)url progress:(BOOL (^)(NSUInteger))progress;
 -(BOOL)sendImage:(UIImage*)image;
--(BOOL)sendImage:(UIImage*)image progress:(BOOL (^)(NSUInteger))progress sizeptr:(NSUInteger*)sizeptr;
+-(BOOL)sendImage:(UIImage*)image progress:(BOOL (^)(NSUInteger))progress;
 -(BOOL)sendData:(NSData*)data filename:(NSString*)filename;
 -(BOOL)sendData:(NSData*)data filename:(NSString*)filename progress:(BOOL (^)(NSUInteger))progress;
 -(void)disconnect;
--- FileSender/FileSender.m
+++ FileSender/FileSender.m
@@ -63,10 +63,9 @@
     return [self sendData:data filename:filename progress:nil];
 }
 
--(BOOL)sendURL:(NSURL*)url progress:(BOOL (^)(NSUInteger))progress sizeptr:(NSUInteger*)sizeptr {
+-(BOOL)sendURL:(NSURL*)url progress:(BOOL (^)(NSUInteger))progress {
     NSData* data = [[NSData alloc] initWithContentsOfURL:url];
     NSString* filename = [url lastPathComponent];
-    if (sizeptr != NULL) *sizeptr = data.length;
     return [self sendData:data filename:filename progress:progress];
 }
 
@@ -77,11 +76,10 @@
     return [self sendData:data filename:filename progress:nil];
 }
 
--(BOOL)sendImage:(UIImage*)image progress:(BOOL (^)(NSUInteger))progress sizeptr:(NSUInteger*)sizeptr {
+-(BOOL)sendImage:(UIImage*)image progress:(BOOL (^)(NSUInteger))progress {
     NSData* data = UIImageJPEGRepresentation(image, 0);
     NSString* filename = [NSString stringWithFormat:@"IMG-%d.jpg", imageCounter];
     imageCounter++;
-    if (sizeptr != NULL) *sizeptr = data.length;
     return [self sendData:data filename:filename progress:progress];
 }
 
