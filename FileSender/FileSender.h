6ebca1adf1de6676ac6d27c529dbd7ef0bf030e5
Sudhip Nashi <sudhip@nashi.us>
2021-01-25 22:01:23 -0600

    Add progress options to all send methods in FileSender

--- FileSender/FileSender.h
+++ FileSender/FileSender.h
@@ -2,6 +2,7 @@
 -(id)init;
 -(BOOL)sendURL:(NSURL*)url;
 -(BOOL)sendImage:(UIImage*)image;
+-(BOOL)sendImage:(UIImage*)image progress:(BOOL (^)(NSUInteger))progress;
 -(BOOL)sendData:(NSData*)data filename:(NSString*)filename;
 -(BOOL)sendData:(NSData*)data filename:(NSString*)filename progress:(BOOL (^)(NSUInteger))progress;
 -(void)disconnect;
--- FileSender/FileSender.m
+++ FileSender/FileSender.m
@@ -60,14 +60,27 @@
 -(BOOL)sendURL:(NSURL*)url {
     NSData* data = [[NSData alloc] initWithContentsOfURL:url];
     NSString* filename = [url lastPathComponent];
-    return [self sendData:data filename:filename];
+    return [self sendData:data filename:filename progress:nil];
+}
+
+-(BOOL)sendURL:(NSURL*)url progress:(BOOL (^)(NSUInteger))progress {
+    NSData* data = [[NSData alloc] initWithContentsOfURL:url];
+    NSString* filename = [url lastPathComponent];
+    return [self sendData:data filename:filename progress:progress];
 }
 
 -(BOOL)sendImage:(UIImage*)image {
     NSData* data = UIImageJPEGRepresentation(image, 0);
     NSString* filename = [NSString stringWithFormat:@"IMG-%d.jpg", imageCounter];
     imageCounter++;
-    return [self sendData:data filename:filename];
+    return [self sendData:data filename:filename progress:nil];
+}
+
+-(BOOL)sendImage:(UIImage*)image progress:(BOOL (^)(NSUInteger))progress {
+    NSData* data = UIImageJPEGRepresentation(image, 0);
+    NSString* filename = [NSString stringWithFormat:@"IMG-%d.jpg", imageCounter];
+    imageCounter++;
+    return [self sendData:data filename:filename progress:progress];
 }
 
 -(BOOL)sendData:(NSData*)data filename:(NSString*)filename progress:(BOOL (^)(NSUInteger))progress {
