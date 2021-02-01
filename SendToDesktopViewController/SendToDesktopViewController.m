798a8ac6a3233784eada5823410a2401ceb365ac
Sudhip Nashi <sudhip@nashi.us>
2021-02-01 11:01:23 -0600

    execute doneBlock and remove view controller in viewWillDisappear instead of in main loop. This is so that in case it is manually dismissed, it gets dismissed faster

--- SendToDesktopViewController/SendToDesktopViewController.m
+++ SendToDesktopViewController/SendToDesktopViewController.m
@@ -228,13 +228,11 @@
     });
 }
 
-// Due to my very not-ideal way of avoiding multithreaded race conditions, the UIActivityVC will
-// still be dismissed. This is because the main file transferring block (above) still goes on
-// even after the SendToDesktop VC is dismissed, and in that block it eventually dismisses
-// the UIActivityVC in the end.
 -(void)viewWillDisappear:(BOOL)animated {
+    dismissControllerInAnotherMethod = YES;
     [self cleanUpAndDisconnect];
-    return [super viewWillDisappear:animated];
+    [super viewWillDisappear:animated];
+    self.doneBlock();
 }
 
 @end
